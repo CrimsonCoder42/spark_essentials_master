a30930b4a2b92b56f87ff3c9f791bc9328b6433c
CrimsonCoder42 <nostro37@gmail.com>
2022-08-30 07:58:52 -0400

    DataFrame columns and expressions

--- src/main/scala/part2dataframesDJA/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframesDJA/ColumnsAndExpressions.scala
@@ -1,7 +1,8 @@
 package part2dataframesDJA
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, column}
+import org.apache.spark.sql.functions.{col, column, expr}
+import part3typesdatasets.Datasets.guitarPlayerBandsDS.selectExpr
 
 object ColumnsAndExpressions extends App {
 
@@ -23,11 +24,54 @@ object ColumnsAndExpressions extends App {
   val carNamesDF = carsDF.select(firstColumn)
 
   // various select methods
-  import spark.implicits
+  import spark.implicits._
   carsDF.select(
     carsDF.col("Name"),
     col("Acceleration"),
-    column("Weight_in_lbs")
+    column("Weight_in_lbs"),
+    'Year, // Scala Symbol, auto-converted to column
+    $"Horsepower", // fancier interpolated string, returns a Column object
   )
+
+  // select with plain column names
+  carsDF.select("Name", "Year")
+
+  //EXPRESSIONS
+  val simplestExpression = carsDF.col("Weight_in_lbs")
+  val weightInKgExpression = carsDF.col("Weight_in_lbs") / 2.2
+
+  val carsWithWeightsDF = carsDF.select(
+    col("Name"),
+    col("Weight_in_lbs"),
+    weightInKgExpression.as("Weight_in_kg"),
+    expr("Weight_in_lbs / 2.2").as("Weight_in_kg_2")
+  )
+  carsWithWeightsDF.show()
+
+  // selectExpr
+
+  val carsWithSelectExprWeightsDF = carsDF.selectExpr(
+    "Name",
+    "Weight_in_lbs",
+    "Weight_in_lbs / 2.2"
+  )
+
+  // DF processing
+  val carsWithKg3DF = carsDF.withColumn("Weight_in_kg_3", col("Weight_in_lbs") / 2.2)
+
+  //renaming a column
+  val carsWithColumnRenamed = carsDF.withColumnRenamed("Weight_in_lbs", "Weight in pounds")
+
+  // careful with column names
+  carsWithColumnRenamed.selectExpr("`Weight in pounds`")
+
+  // remove a column
+  carsWithColumnRenamed.drop("Cylinders", "Displacement")
+
+  // filtering
+  val europeanCarsDF = carsDF.filter(col("Origin") =!= "USA")
+  val europeanCarsDF2 = carsDF.where(col("Origin") =!= "USA")
+
+  //filtering with expression strings
 
 }
